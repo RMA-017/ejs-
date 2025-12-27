bcbf25cd9a87268d00be9c25a3911b2f0210d28b
Muhammad ALI <rmuhammadali1993@gmail.com>
2025-12-27 17:44:15 +0500

    ejs

--- README.txt
+++ README.txt
@@ -1,34 +1,31 @@
-Escape Velocity by HTML5 UP
+Multiverse by HTML5 UP
 html5up.net | @ajlkn
 Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 
 
-A new responsive template featuring a flat (but not too flat) minimalistic design, spacious
-layout, and styling for all basic page elements. Its demo images* are courtesy of the supremely
-talented photographer Felicia Simion. If you like photography or just enjoy being blown away by
-awesome stuff, check out her portfolio for more stunning images:
+Say hello to Multiverse, a slick, one-page gallery design with a fully functional lightbox
+(courtesy of my Poptrox plugin for jQuery) and a custom, reusable "panel" system (click the
+"About" button in the lower right to see what I mean). Had a ton of fun putting this one
+together, and I hope you have as much fun working with it :)
 
-http://ineedchemicalx.deviantart.com/
+Demo images* courtesy of Unsplash, a radtastic collection of CC0 (public domain) images
+you can use for pretty much whatever.
 
-(* = Not included! Only meant for use with my own on-site demo, so please do NOT download
-and/or use any of Felicia's work without her explicit permission!)
-
-Feedback, bug reports, and comments are not only welcome, but strongly encouraged :)
+(* = not included)
 
 AJ
 aj@lkn.io | @ajlkn
-
-PS: Not sure how to get that contact form working? Give formspree.io a try (it's awesome).
 
 
 Credits:
 
 	Demo Images:
-		Felicia Simion (ineedchemicalx.deviantart.com)
+		Unsplash (unsplash.com)
 
 	Icons:
 		Font Awesome (fontawesome.io)
 
 	Other:
 		jQuery (jquery.com)
+		Poptrox (github.com/ajlkn/jquery.poptrox)
 		Responsive Tools (github.com/ajlkn/responsive-tools)
--- index.js
+++ index.js
@@ -1,21 +1,107 @@
+import path from "path"
 import express from "express"
-import { promises as fs } from "fs"
+import cors from "cors"
 import ejs from "ejs"
+import { promises as fs } from "fs"
 
 const app = express()
 app.use(express.json())
+app.use(cors())
+app.use(express.static("public"));
 
-app.get("/", async (req, resp) => {
-    let homePage = await fs.readFile("./index.html", "utf8")
-    resp.send(homePage)
+let html = await fs.readFile("./index.html", "utf-8")
+
+app.get("/", async (req, res) => {
+    res.set("Content-Type", "text/html")
+    res.send(html)
 })
 
-app.get("/assets/:fileName", async (req, resp) => {
-    let cssData = await fs.readFile(`./assets/${req.params.fileName}`, "utf8")
-    resp.set("content-type", "text/css")
-    resp.send(cssData)
+app.get("/index.html", async (req, res) => {
+    res.set("Content-Type", "text/html")
+    res.send(html)
 })
+
+app.get("/assets/css/:roadName", async (req, res) => {
+    let data = await fs.readFile(`./assets/css/${req.params.roadName}`, "utf-8")
+    res.set("Content-Type", "text/css")
+    res.send(data)
+})
+
+app.get("/assets/css/images/:roadName", async (req, res) => {
+    let data = await fs.readFile(`./assets/css/images/${req.params.roadName}`, "utf-8")
+    res.set("Content-Type", "text/svg")
+    res.send(data)
+})
+
+app.get("/assets/js/:roadName", async (req, res) => {
+    let data = await fs.readFile(`./assets/js/${req.params.roadName}`, "utf-8")
+    res.set("Content-Type", "text/js")
+    res.send(data)
+})
+
+app.get("/assets/sass/:roadName", async (req, res) => {
+    let data = await fs.readFile(`./assets/sass/${req.params.roadName}`, "utf-8")
+    res.set("Content-Type", "text/css")
+    res.send(data)
+})
+
+app.get("/assets/sass/base/:roadName", async (req, res) => {
+    let data = await fs.readFile(`./assets/sass/base/${req.params.roadName}`, "utf-8")
+    res.set("Content-Type", "text/css")
+    res.send(data)
+})
+
+app.get("/assets/sass/components/:roadName", async (req, res) => {
+    let data = await fs.readFile(`./assets/sass/components/${req.params.roadName}`, "utf-8")
+    res.set("Content-Type", "text/css")
+    res.send(data)
+})
+
+app.get("/assets/sass/layout/:roadName", async (req, res) => {
+    let data = await fs.readFile(`./assets/sass/layout/${req.params.roadName}`, "utf-8")
+    res.set("Content-Type", "text/css")
+    res.send(data)
+})
+
+app.get("/assets/sass/libs/:roadName", async (req, res) => {
+    let data = await fs.readFile(`./assets/sass/libs/${req.params.roadName}`, "utf-8")
+    res.set("Content-Type", "text/css")
+    res.send(data)
+})
+
+app.get("/images/fulls/:roadName", async (req, res) => {
+    let data = await fs.readFile(`./images/fulls/${req.params.roadName}`)
+    res.set("Content-Type", "image/jpg")
+    res.send(data)
+})
+
+app.get("/images/thumbs/:roadName", async (req, res) => {
+    let data = await fs.readFile(`./images/thumbs/${req.params.roadName}`)
+    res.set("Content-Type", "image/jpg")
+    res.send(data)
+})
+
+
+app.get("/assets/webfonts/:fontName", async (req, res) => {
+    try {
+        const data = await fs.readFile(`./assets/webfonts/${req.params.fontName}`);
+
+        let contentType;
+        if (req.params.fontName.endsWith(".eot")) contentType = "application/vnd.ms-fontobject";
+        else if (req.params.fontName.endsWith(".ttf")) contentType = "font/ttf";
+        else if (req.params.fontName.endsWith(".woff")) contentType = "font/woff";
+        else if (req.params.fontName.endsWith(".woff2")) contentType = "font/woff2";
+        else contentType = "application/octet-stream";
+
+        res.set("Content-Type", contentType);
+        res.send(data);
+
+    } catch (err) {
+        res.status(404).send("Font not found");
+    }
+});
+
 
 app.listen(3000, () => {
-    console.log(`server on port: 3000`);
+    console.log("server on 3000");
 })
