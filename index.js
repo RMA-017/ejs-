0788b07fd3af9e6c8577090ff4bcf77055725923
Muhammad ALI <rmuhammadali1993@gmail.com>
2025-12-28 13:14:53 +0500

    cars

--- index.js
+++ index.js
@@ -1,107 +1,96 @@
-import path from "path"
 import express from "express"
-import cors from "cors"
-import ejs from "ejs"
 import { promises as fs } from "fs"
+import ejs from "ejs"
 
 const app = express()
-app.use(express.json())
-app.use(cors())
-app.use(express.static("public"));
+app.use("/assets", express.static("assets"))
+app.use("/images", express.static("images"))
 
-let html = await fs.readFile("./index.html", "utf-8")
+let features = ["Consequat tempus", "Etiam adipiscing", "Libero nullam", "Blandit condimentum", "Lorem ipsum dolor", "Nibh amet venenatis"]
+
+let gallery = [
+    {
+        image: {
+            fulls: "images/gallery/fulls/01.jpg",
+            thumbs: "images/gallery/thumbs/01.jpg"
+        }
+    },
+    {
+        image: {
+            fulls: "images/gallery/fulls/02.jpg",
+            thumbs: "images/gallery/thumbs/02.jpg"
+        }
+    },
+    {
+        image: {
+            fulls: "images/gallery/fulls/03.jpg",
+            thumbs: "images/gallery/thumbs/03.jpg"
+        }
+    },
+    {
+        image: {
+            fulls: "images/gallery/fulls/04.jpg",
+            thumbs: "images/gallery/thumbs/04.jpg"
+        }
+    },
+
+]
+
+let gallery2 = [
+    {
+        image: {
+            rasm5: "images/gallery/fulls/05.jpg",
+            rasm55: "images/gallery/thumbs/05.jpg"
+        }
+    },
+    {
+        image: {
+            rasm5: "images/gallery/fulls/06.jpg",
+            rasm55: "images/gallery/thumbs/06.jpg"
+        }
+    },
+    {
+        image: {
+            rasm5: "images/gallery/fulls/07.jpg",
+            rasm55: "images/gallery/thumbs/07.jpg"
+        }
+    }
+]
+
+let gallery3 = [
+    {
+        image: {
+            rasm5: "images/gallery/fulls/08.jpg",
+            rasm55: "images/gallery/thumbs/08.jpg"
+        }
+    },
+    {
+        image: {
+            rasm5: "images/gallery/fulls/09.jpg",
+            rasm55: "images/gallery/thumbs/09.jpg"
+        }
+    },
+    {
+        image: {
+            rasm5: "images/gallery/fulls/10.jpg",
+            rasm55: "images/gallery/thumbs/10.jpg"
+        }
+    }
+]
 
 app.get("/", async (req, res) => {
+    let data = await fs.readFile("./index.html", "utf-8")
+    let result = ejs.render(data, {
+        features,
+        gallery,
+        gallery2,
+        gallery3
+    })
     res.set("Content-Type", "text/html")
-    res.send(html)
+    res.send(result)
 })
-
-app.get("/index.html", async (req, res) => {
-    res.set("Content-Type", "text/html")
-    res.send(html)
-})
-
-app.get("/assets/css/:roadName", async (req, res) => {
-    let data = await fs.readFile(`./assets/css/${req.params.roadName}`, "utf-8")
-    res.set("Content-Type", "text/css")
-    res.send(data)
-})
-
-app.get("/assets/css/images/:roadName", async (req, res) => {
-    let data = await fs.readFile(`./assets/css/images/${req.params.roadName}`, "utf-8")
-    res.set("Content-Type", "text/svg")
-    res.send(data)
-})
-
-app.get("/assets/js/:roadName", async (req, res) => {
-    let data = await fs.readFile(`./assets/js/${req.params.roadName}`, "utf-8")
-    res.set("Content-Type", "text/js")
-    res.send(data)
-})
-
-app.get("/assets/sass/:roadName", async (req, res) => {
-    let data = await fs.readFile(`./assets/sass/${req.params.roadName}`, "utf-8")
-    res.set("Content-Type", "text/css")
-    res.send(data)
-})
-
-app.get("/assets/sass/base/:roadName", async (req, res) => {
-    let data = await fs.readFile(`./assets/sass/base/${req.params.roadName}`, "utf-8")
-    res.set("Content-Type", "text/css")
-    res.send(data)
-})
-
-app.get("/assets/sass/components/:roadName", async (req, res) => {
-    let data = await fs.readFile(`./assets/sass/components/${req.params.roadName}`, "utf-8")
-    res.set("Content-Type", "text/css")
-    res.send(data)
-})
-
-app.get("/assets/sass/layout/:roadName", async (req, res) => {
-    let data = await fs.readFile(`./assets/sass/layout/${req.params.roadName}`, "utf-8")
-    res.set("Content-Type", "text/css")
-    res.send(data)
-})
-
-app.get("/assets/sass/libs/:roadName", async (req, res) => {
-    let data = await fs.readFile(`./assets/sass/libs/${req.params.roadName}`, "utf-8")
-    res.set("Content-Type", "text/css")
-    res.send(data)
-})
-
-app.get("/images/fulls/:roadName", async (req, res) => {
-    let data = await fs.readFile(`./images/fulls/${req.params.roadName}`)
-    res.set("Content-Type", "image/jpg")
-    res.send(data)
-})
-
-app.get("/images/thumbs/:roadName", async (req, res) => {
-    let data = await fs.readFile(`./images/thumbs/${req.params.roadName}`)
-    res.set("Content-Type", "image/jpg")
-    res.send(data)
-})
-
-
-app.get("/assets/webfonts/:fontName", async (req, res) => {
-    try {
-        const data = await fs.readFile(`./assets/webfonts/${req.params.fontName}`);
-
-        let contentType;
-        if (req.params.fontName.endsWith(".eot")) contentType = "application/vnd.ms-fontobject";
-        else if (req.params.fontName.endsWith(".ttf")) contentType = "font/ttf";
-        else if (req.params.fontName.endsWith(".woff")) contentType = "font/woff";
-        else if (req.params.fontName.endsWith(".woff2")) contentType = "font/woff2";
-        else contentType = "application/octet-stream";
-
-        res.set("Content-Type", contentType);
-        res.send(data);
-
-    } catch (err) {
-        res.status(404).send("Font not found");
-    }
-});
 
 
 app.listen(3000, () => {
-    console.log("server on 3000");
+    console.log("3000");
 })
